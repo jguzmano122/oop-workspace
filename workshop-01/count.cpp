003721e81f00d50f38207b4401432e23cfd698ff
Jguzmano122 <a1838934@student.adelaide.edu.au>
2024-07-24 20:01:49 +0930

    clearing all

---
+++ workshop-01/count.cpp
@@ -0,0 +1,12 @@
+// JG function file JG: add the even numbers in given array
+
+int count(int array[], int n) {
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        if (array[i] %2 == 0) {
+            total +=1;
+        }
+    }
+
+    return total;
+}
