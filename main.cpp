29fac7e5c94a3858b7c1317704ee3581d39e3435
Jguzmano122 <a1838934@student.adelaide.edu.au>
2024-07-24 21:16:28 +0930

    files for workshop - revised 2

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ extern int count(int[], int);
 
 int main() {
     int array[5] = {4,5,6,7,8};
-    std::cout << "The sum of EVEN numbers in the array is: " << count(array, 5) << std::endl;
+    std::cout << "The number is: " << count(array, 5) << std::endl;
     return 0;
 }
