149877835c83ad15c0fb5cd8de58e7fae635a52d
Jguzmano122 <a1838934@student.adelaide.edu.au>
2024-08-02 22:39:38 +0930

    another take

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 void print_summed(int array1[3][3],int array2[3][3]){
 
-int array3 [3][3] ={0, 0, 0};
+int array3 [3][3] ={};
 
 for (int i =0; i < 3; i++) {
 
     for (int j = 0; j < 3; j ++){
     
-        array3 [i][j] = array1 [i][j] * array2 [i][j] ;
+        array3 [i][j] = array1 [i][j] + array2 [i][j] ;
         cout << array3[i][j] << " ";
+
     } cout << endl;
 
     } 
