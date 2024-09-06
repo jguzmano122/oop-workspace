c658311e270d973294aa0a26ed86179c1bcd156b
Jguzmano122 <a1838934@student.adelaide.edu.au>
2024-09-06 16:01:20 +0930

    iii

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -8,8 +8,15 @@ int main () {
 
     AirVehicle av1;
 
-    cout  << av1.get_weight () << av1.get_fuel() << av1.get_numberOfFlights () << endl;
+    cout  << "weight: " << av1.get_weight () << " fuel: " << av1.get_fuel() << " No. flights: " << av1.get_numberOfFlights () << endl;
 
+
+    av1.set_numberOfFlights (32);
+    av1.set_weight (2200);
+    av1.fly (10, 10);
+
+
+     cout  << "weight: " << av1.get_weight () << " fuel: " << av1.get_fuel() << " No. flights: " << av1.get_numberOfFlights () << endl;
 
 
     return 0;
