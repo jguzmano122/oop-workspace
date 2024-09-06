5fa95081163591deab333c7c6f0371b3b80e7103
Jguzmano122 <a1838934@student.adelaide.edu.au>
2024-09-06 16:20:02 +0930

    comment main

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -6,17 +6,17 @@ using namespace std;
 
 int main () {
 
-    AirVehicle av1;
+    // AirVehicle av1;
 
-    cout  << "weight: " << av1.get_weight () << " fuel: " << av1.get_fuel() << " No. flights: " << av1.get_numberOfFlights () << endl;
+    // cout  << "weight: " << av1.get_weight () << " fuel: " << av1.get_fuel() << " No. flights: " << av1.get_numberOfFlights () << endl;
 
 
-    av1.set_numberOfFlights (32);
-    av1.set_weight (2200);
-    av1.fly (10, 10);
-    av1.fly (10, 10);
+    // av1.set_numberOfFlights (32);
+    // av1.set_weight (2200);
+    // av1.fly (10, 10);
+    // av1.fly (10, 10);
 
-     cout  << "weight: " << av1.get_weight () << " fuel: " << av1.get_fuel() << " No. flights: " << av1.get_numberOfFlights () << endl;
+    //  cout  << "weight: " << av1.get_weight () << " fuel: " << av1.get_fuel() << " No. flights: " << av1.get_numberOfFlights () << endl;
 
     return 0;
 }
