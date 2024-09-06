1b6335f11079e754592f88aa0c0e7b35bf945a1f
Jguzmano122 <a1838934@student.adelaide.edu.au>
2024-09-06 16:15:50 +0930

    removed gch

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -14,10 +14,9 @@ int main () {
     av1.set_numberOfFlights (32);
     av1.set_weight (2200);
     av1.fly (10, 10);
-
+    av1.fly (10, 10);
 
      cout  << "weight: " << av1.get_weight () << " fuel: " << av1.get_fuel() << " No. flights: " << av1.get_numberOfFlights () << endl;
-
 
     return 0;
 }
